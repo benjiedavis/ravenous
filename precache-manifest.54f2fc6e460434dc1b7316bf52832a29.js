11451d7f51944c8b3b6d91722d72ab20747fe69b
Benjie Davis <benjiedavis@users.noreply.github.com>
2020-07-15 11:33:03 -0400

    Updates

---
+++ precache-manifest.54f2fc6e460434dc1b7316bf52832a29.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8a89d0a724b7b090300e54f1b5e406",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "edfd2bb2761d68106a51",
+    "url": "/ravenous/static/css/main.7be6086e.chunk.css"
+  },
+  {
+    "revision": "9c1af7b737472adcb6b4",
+    "url": "/ravenous/static/js/2.34c6e3d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.34c6e3d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edfd2bb2761d68106a51",
+    "url": "/ravenous/static/js/main.23889f89.chunk.js"
+  },
+  {
+    "revision": "9e12bad58d82799d27a7",
+    "url": "/ravenous/static/js/runtime-main.b1d203fc.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ravenous/static/media/logo.5d5d9eef.svg"
+  }
+]);
